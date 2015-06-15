db40da45acb70f8b92b0822e604ba2644cc6e58f
JIKLorz <aida@s14.yeskist.net>
2015-06-15 10:21:42 +0900

    時間帯に応じて返答を変える

--- talk.js
+++ talk.js
@@ -1,5 +1,12 @@
+	var date = new Date();			/* 日時に関する情報を取得 */
+	var hours = date.getHours();	/* 何時か取得 */
+
 function tbox1(){
-	var str = document.js.Take.value;
-	
-	alert(str);
+	if (hours >= 5 && hours <= 10){
+		document.Form.Reply.value = 'おはよう';
+	} else if (hours > 10 && hours <= 17){
+		document.Form.Reply.value = 'こんにちは';
+	} else {
+		document.Form.Reply.value = 'こんばんは';
+	}
 }
