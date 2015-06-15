867a09b8ed61433e751b2b9f1baad8444a22b8db
JIKLorz <aida@s14.yeskist.net>
2015-06-15 12:19:30 +0900

    時間ごとに挨拶の返答を変える

--- talk.js
+++ talk.js
@@ -1,12 +1,29 @@
 	var date = new Date();			/* 日時に関する情報を取得 */
 	var hours = date.getHours();	/* 何時か取得 */
-
+	var comreply = document.Form.Talk.value;
+	var Goodmorning = 'おはようございます';
+	var Hello = 'こんにちは';
+	var Goodafternoon = 'こんばんは';
+	var reply;
+	
+/* メインメソッド */
 function tbox1(){
+	
+	if (comreply == Goodmorning || comreply == Hello || comreply == Goodafternoon){
+		reply = comReply();		/* 正しい挨拶なら正しい返答 */
+	}
+}
+
+
+/* 挨拶を判定するメソッド */
+function comReply(){
+	
 	if (hours >= 5 && hours <= 10){
-		document.Form.Reply.value = 'おはよう';
-	} else if (hours > 10 && hours <= 17){
+		document.Form.Reply.value = 'おはようございます';
+	} else if (hours >= 11 && hours <= 17){
 		document.Form.Reply.value = 'こんにちは';
 	} else {
 		document.Form.Reply.value = 'こんばんは';
 	}
+	
 }
