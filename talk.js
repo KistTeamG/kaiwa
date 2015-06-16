dce3070f18d4eb9a0fc96443e3bae17a84fd9893
JIKLorz <aida@s14.yeskist.net>
2015-06-16 13:00:19 +0900

    空白・改行だけでも返答する

--- talk.js
+++ talk.js
@@ -1,20 +1,17 @@
 	var date = new Date();			/* 日時に関する情報を取得 */
 	var hours = date.getHours();	/* 何時か取得 */
 	var comreply = document.Form.Talk.value;
-	var Goodmorning = 'おはようございます';
-	var Hello = 'こんにちは';
-	var Goodafternoon = 'こんばんは';
 	
 /* メインメソッド */
 function tbox1(){
 	
 	document.Form.target = "_self";		/* そのページのまま表示 */
 	
-	if (document.Form.Talk.value =~ "おはよう" || document.Form.Talk.value =~ "こんにちは" || document.Form.Talk.value =~ "こんばんは"){
+	if (document.Form.Talk.value.match("おはよう") || document.Form.Talk.value.match("こんにちは") || document.Form.Talk.value.match("こんばんは")){
 		document.Form.Reply.value = comReply();
 		document.Form.Talk.value = null;
-	} else if (comreply == null){
-		document.Form.Reply.value = null;
+	} else if (comreply == null || comreply == "\n"){
+		document.Form.Reply.value = "なんですか？";
 	}
 }
 
