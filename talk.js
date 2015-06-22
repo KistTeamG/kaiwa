701214c928e2046bd9c56a5958497778b72c3d9f
JIKLorz <aida@s14.yeskist.net>
2015-06-22 10:48:40 +0900

    jqueryに対応

--- talk.js
+++ talk.js
@@ -1,20 +1,18 @@
 	var date = new Date();			/* 日時に関する情報を取得 */
 	var hours = date.getHours();	/* 何時か取得 */
-	var comreply = document.Form.Talk.value;
+	var comreply = $("#Reply").val();
 	
 /* メインメソッド */
-function tbox1(){
-	
-	document.Form.target = "_self";		/* そのページのまま表示 */
-	
-	if (document.Form.Talk.value.match("おはよう") || document.Form.Talk.value.match("こんにちは") || document.Form.Talk.value.match("こんばんは")){
-		document.Form.Reply.value = comReply();
-		document.Form.Talk.value = null;
-	} else if (comreply == null || comreply == "\n"){
-		document.Form.Reply.value = "なんですか？";
-	}
-}
-
+$(function() {
+	$("#tbox").click(function() {
+		if ($("#Talk").val().match("おはよう") || $("#Talk").val().match("こんにちは")|| $("#Talk").val().match("こんばんは")){
+			$("#Reply").val(comReply() + "\n")/* + Temperature()*/;
+		} else if (comreply == null || comreply == "\n"){
+			$("#Reply").val("なんですか？");
+		}
+		$("#Talk").val(null);
+	});
+});
 
 /* 挨拶を判定するメソッド */
 function comReply(){
@@ -29,4 +27,17 @@ function comReply(){
 	}
 	
 	return reply;
+}
+
+/* 気温に関するメソッド */
+function Temperature(){
+	uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=170010';	/* 金沢の天気情報を取得 */
+	
+	var temperature = forecasts.temp;
+	var tempReply;
+	
+	if (temperature >= 26 || temperature <= 40){
+		tempReply = '暑い';
+	}
+	return tempReply;
 }
