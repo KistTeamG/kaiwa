f1e2297bf701b2e291670d85d2ab975afcc0b21f
JIKLorz <aida@s14.yeskist.net>
2015-07-13 10:45:27 +0900

    Formを送信していないときに話しかてくる

--- talk.js
+++ talk.js
@@ -1,5 +1,7 @@
 	var date = new Date();			/* 日時に関する情報を取得 */
 	var hours = date.getHours();	/* 何時か取得 */
+	var second = 1000;
+	var auto = setInterval('autoTalk()', second * 10);	/* 10秒ごとに話しかけてくる */
 	var comreply = $("#Reply").val();
 	var reply = ["おはよう", "こんにちは", "こんばんは"];	/* 挨拶の配列 */
 	var temp = ["暑", "寒"];	/* 暑さの配列 */
@@ -9,15 +11,15 @@
 /* メインメソッド */
 $(function() {
 	$("#Reply").prop("disabled", true);
+	auto;
 	$("#tbox").click(function() {
-		setTimeout('autoTalk()', 10000);	/* 10秒ごとに話しかけてくる */
 		comTalk();
 	});
 });
 
 /* 返答メソッド */
 function comTalk(){
-	
+
 	if ($("#Talk").val().match(".*\n") || $("#Talk").val().match(".")){
 		$("#Reply").val("どうしたの？");
 	} else if ($("#Talk").val(null) || $("#Talk").val("\n")){
@@ -42,6 +44,7 @@ function comTalk(){
 		}
 	}
 	$("#Talk").val(null);
+	clearInterval(auto);
 }
 
 /* 挨拶を判定するメソッド */
@@ -63,12 +66,12 @@ function comReply(){
 /* 暑さを判定(ランダム)するメソッド */
 function comTemp(){
 	
-	var temp = Math.floor(Math.random() * 40);
+	var rand = Math.floor(Math.random() * 40);
 	var tempReply;
 	
-	if (temp >= 30 && temp <= 40){
+	if (rand >= 30 && rand <= 40){
 		tempReply = '暑いです';
-	} else if (temp >= 20 && temp < 30){
+	} else if (rand >= 20 && rand < 30){
 		tempReply = 'ちょうど良いです';
 	} else {
 		tempReply = '寒いです';
@@ -78,16 +81,18 @@ function comTemp(){
 
 /* 自動で会話を行うメソッド */
 function autoTalk(){
-	var reply;
+	var talk;
 	var rand = 0;
 	
 	if (rand < autoReply.length){
 		rand = Math.floor(Math.random() * autoReply.length);
 	}
-	reply = $("#Reply").val(autoReply[rand]);
+	talk = $("#Reply").val(autoReply[rand]);
 
-	return reply;
+	return talk;
 }
+
+
 /* タグを閉じるメソッド */
 function paseClose(){
 		window.open('about:blank','_self').close();
